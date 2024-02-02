17dff924d55f4e940295bef2e264778e8f34d07f
JohnnyDeng6 <jzydeng@gmail.com>
2024-02-02 13:32:48 -0800

    added checklist

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Checklist from './pages/Checklist.js';
+import Home from './pages/Home.js';
+
 
 export default function Main() {
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Home />} />
+      <Route path="/checklist" element={<Checklist />} />
     </Routes>
   );
 }
---
+++ client/src/pages/Checklist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Checklist = () => {
+  return (
+    <div>
+      <h1>Hello From Dashboard!</h1>
+    </div>
+  );
+}
+
+export default Checklist;
