1cabdbf7380cac17b2baf368f9a588820b0e8d5e
Nakul Bansal <nakulbansal727@gmail.com>
2024-02-02 13:12:24 -0800

    Initialized frontend file structure

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+// index.js - components
+// This file will act as the import for all functions inside the directory
+// common - general layouts like header, footer, etc.
+// helper - utility function specific to app like rendering components
