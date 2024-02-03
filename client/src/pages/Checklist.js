e03a1230a590735e923d3ce3e31c367c2bbcc5dc
JohnnyDeng6 <jzydeng@gmail.com>
2024-02-02 16:59:12 -0800

    checklist

--- client/src/pages/Checklist.js
+++ client/src/pages/Checklist.js
@@ -21,15 +21,51 @@ import { useNavigate } from 'react-router-dom';
 const Checklist = (props) => {
   const [Tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [name, setname] = useState('');
+  const [msg, setmsg] = useState('');
+  const [uid, setuid] = useState(props);
+  
+  const getTasks = async (e) => {
+    const token = localStorage.getItem('jwt');
+    e.preventDefault();
+    try {
+      const result = await fetch('http://localhost:3000/api/tasks');
+      
+      const data = await response.json();
+      setTasks(data);
 
 
-  const handleAddTask = (e) => {
-    //send newTask to db
-    //try post 
-
-    //get new (this is a list) 
-    //setTasks(new)
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
   };
+
+
+  const handleAddTask = async (e) => {
+    const token = localStorage.getItem('jwt');
+    e.preventDefault()
+
+    try {
+      const result = await fetch('http://localhost:3000/api/tasks/add', {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+      'Authorization': 'Bearer ${token}',
+      'Content-Type': 'something/json',
+      },
+      body: JSON.stringify({ uid: uid, name: name, msg: msg})
+    })
+    if (result.ok) {
+      getTasks();
+      setNewTasks('');
+    } else {
+      console.error('Failed to add task');
+    }
+  } catch (error) {
+    console.error('Error adding task:', error);
+  }
+};
+
 
   const handleRemoveTask = (tasks) => {
     //remove task from db
@@ -51,7 +87,7 @@ const Checklist = (props) => {
             type="text"
             value={newTask}
             placeholder="Add Task"
-            onChange={(ev) => setNewTask(ev)}
+            onChange={(ev) => setNewTask(ev.target.value)}
            />
            <button type="submit">Add Task</button>
         </form>
@@ -60,12 +96,11 @@ const Checklist = (props) => {
       <div className="list out tasks">
           <ul>
             {Tasks.map((task, index) => (
-            <li key={index}>
-              {task}
+              <li key={index}>{task.name} - {task.message}
               <button onClick={() => handleRemoveTask(index)}>Remove</button>
               <button onClick={() => handleEditTask(index)}>Edit</button>
-
-            </li>
+              </li>
+          
             ))}
           </ul>
       </div>
